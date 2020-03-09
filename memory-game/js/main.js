658f6f15ca36e07f7b2d81149c7b39106c6280a6
Kok_Chuan <kokchuan1996@gmail.com>
2020-03-09 16:56:08 +0800

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,19 +1,42 @@
 console.log("Up and running!");
-const cards = ['queen','queen','king','king'];
+const cards = [
+    {
+    rank: 'queen',
+    suit: 'hearts',
+    cardImage: "images/queen-of-hearts.png"
+    },
+    {
+    rank: 'queen',
+    suit: 'diamond',
+    cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+    rank: 'king',
+    suit: 'hearts',
+    cardImage: "images/king-of-hearts.png"
+    },
+    {
+    rank: 'king',
+    suit: 'diamond',
+    cardImage: "images/king-of-diamonds.png"
+    }
+];
 const cardsInPlay =[];
 checkForMatch=function(){
     if(cardsInPlay.length===2){
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log("You found a match!");
+        alert("You found a match!");
       } else {
-        console.log("Sorry, try again.");
+        alert("Sorry, try again.");
       }
     }
 }
 flipCard =function(cardId){
-    cardsInPlay.push(cardId); 
-    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId].rank); 
+    console.log("User flipped " + cards[cardId].rank);
     checkForMatch();
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
         }
 flipCard(0);
 flipCard(2);
